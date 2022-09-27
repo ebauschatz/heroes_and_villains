c40cc52557ae293029834a790116decf519e71b0
ebauschatz <eaenglish113@gmail.com>
2022-09-27 16:28:13 -0500

    feat: adding ability to get super by id

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from .models import Super
+from .serializers import SuperSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def super_by_id(request, pk):
+    found_super = get_object_or_404(Super, pk = pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(found_super)
+        return Response(serializer.data, status=status.HTTP_200_OK)
