25682557fdb922a2f656cfd1f013b2fd6888d0d9
ebauschatz <eaenglish113@gmail.com>
2022-09-28 10:13:30 -0500

    feat: adding get endpoints for super types by parameter and custom response for all supers

--- supers/views.py
+++ supers/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from .models import Super
 from .serializers import SuperSerializer
+from super_types.models import SuperType
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -12,11 +13,13 @@ def super_by_id(request, pk):
     if request.method == 'GET':
         serializer = SuperSerializer(found_super)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
     elif request.method == 'PUT':
         serializer = SuperSerializer(found_super, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+
     elif request.method == 'DELETE':
         found_super.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -28,3 +31,20 @@ def supers_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    elif request.method == 'GET':
+        super_type_param = request.query_params.get('type')
+        if super_type_param:
+            supers = Super.objects.filter(super_type__type = super_type_param)
+            serializer = SuperSerializer(supers, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            custom_response = {}
+            super_types = SuperType.objects.all()
+            supers = Super.objects.all()
+            for current_type in super_types:
+                supers_of_type = supers.filter(super_type = current_type.id)
+                serializer = SuperSerializer(supers_of_type, many=True)
+                custom_response[current_type.type] = serializer.data
+            return Response(custom_response, status=status.HTTP_200_OK)
+        
