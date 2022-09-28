86bdd2cc0acccc477fe248af27c35ec250ebea9d
ebauschatz <eaenglish113@gmail.com>
2022-09-28 13:56:36 -0500

    feat: adding M2M powers field on super and Patch endpoint to link super to power

--- supers/urls.py
+++ supers/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('<int:pk>/', views.super_by_id),
     path('', views.supers_list),
+    path('super/<int:super_pk>/power/<int:power_pk>/', views.add_power_to_super)
 ]
